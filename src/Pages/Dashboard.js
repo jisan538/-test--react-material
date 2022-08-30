9ab83805342cd1f1a1e096b23f676c983006dcf8
jisan538 <sadman.sakib@intelli.global>
2022-08-30 18:37:32 +0600

    card

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,29 +1,37 @@
-import { Box, Toolbar, Typography } from "@mui/material";
+import { Box, Card, Grid, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import Sidebar from "../Components/Sidebar";
+import BarChart from "./Components/BarChart";
 import StateCard from "./Components/StateCard";
 
 export default function Dashboard() {
-  const drawerWidth = 280;
+    const drawerWidth = 280;
 
-  return (
-    <Box>
-      <Sidebar />
-      <Box
-        component="main"
-        sx={{
-          p: 3,
-          width: { lg: `calc(100% - ${drawerWidth}px)` },
-          ml: { lg: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar />
-        <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, Welcome back
-        </Typography>
-        <StateCard sx={{ mb: 5 }} />
-        
-      </Box>
-    </Box>
-  );
+    return (
+        <Box>
+            <Sidebar />
+            <Box
+                component="main"
+                sx={{
+                    p: 3,
+                    width: { lg: `calc(100% - ${drawerWidth}px)` },
+                    ml: { lg: `${drawerWidth}px` },
+                }}
+            >
+                <Toolbar />
+                <Typography variant="h4" sx={{ mb: 5 }}>
+                    Hi, Welcome back
+                </Typography>
+                <StateCard sx={{ mb: 5 }} />
+                <Grid container spacing={2} sx={{ mt: 5 }}>
+                    <Grid item xs={12} sm={12} md={8}>
+                        <Card sx={{ p: 5 }}></Card>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <Card sx={{ p: 5 }}></Card>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
+    );
 }
