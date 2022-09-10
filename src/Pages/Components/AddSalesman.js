2adbad28f82ba8e7d8f2741e41f8300457edfc39
jisan538 <sadman.sakib@intelli.global>
2022-09-10 21:20:49 +0600

    salesman add page

--- src/Pages/Components/AddSalesman.js
+++ src/Pages/Components/AddSalesman.js
@@ -1,7 +1,25 @@
-import { Box, Modal, Typography } from "@mui/material";
-import React from "react";
+import {
+    Box,
+    Button,
+    IconButton,
+    Modal,
+    TextField,
+    Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import ImageUploading from "react-images-uploading";
+import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
+import ReplayOutlinedIcon from "@mui/icons-material/ReplayOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 export default function AddSalesman({ open, handleClose }) {
+    const [images, setImages] = useState([]);
+    const maxNumber = 69;
+    const onChange = (imageList, addUpdateIndex) => {
+        // data for submit
+        setImages(imageList);
+        console.log(imageList, addUpdateIndex);
+    };
     return (
         <Modal
             open={open}
@@ -15,18 +33,114 @@ export default function AddSalesman({ open, handleClose }) {
                     top: "50%",
                     left: "50%",
                     transform: "translate(-50%, -50%)",
-                    width: 400,
-                    position: 'absolute',
+                    width: 600,
+                    position: "absolute",
                     p: 4,
                 }}
             >
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
+                <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                    sx={{ mb: 4, textAlign: "center" }}
+                >
+                    Salesman Details
                 </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor
-                    ligula.
-                </Typography>
+                <Box>
+                    <Box sx={{display: "flex", justifyContent: "center",alignItems: "center", mb:5}}>
+                        <ImageUploading
+                            multiple
+                            value={images}
+                            onChange={onChange}
+                            maxNumber={maxNumber}
+                            dataURLKey="data_url"
+                            acceptType={["jpg"]}
+                        >
+                            {({ 
+                                imageList,
+                                onImageUpload,
+                                onImageUpdate,
+                                onImageRemove,
+                            }) => (
+                                // write your building UI
+                                <Box className="upload__image-wrapper">
+                                    <Button onClick={onImageUpload}>
+                                        Upload Image <FileUploadOutlinedIcon />
+                                    </Button>
+                                    &nbsp;
+                                    {imageList.map((image, index) => (
+                                        <Box key={index} className="image-item" sx={{display: "flex",flexDirection: "column", justifyContent: "center",alignItems: "center"}}>
+                                            <img
+                                                src={image.data_url}
+                                                alt=""
+                                                width="150
+"
+                                                style={{marginTop:"24px",marginBottom:"24px"}}
+                                            />
+                                            <Box className="image-item__btn-wrapper" sx={{display: "flex", justifyContent: "center",alignItems: "center"}}>
+                                                <IconButton
+                                                    onClick={() =>
+                                                        onImageUpdate(index)
+                                                    }
+                                                >
+                                                    <ReplayOutlinedIcon />
+                                                </IconButton>
+                                                <IconButton
+                                                    onClick={() =>
+                                                        onImageRemove(index)
+                                                    }
+                                                >
+                                                    <DeleteOutlinedIcon sx={{color:"red"}}/>
+                                                </IconButton>
+                                            </Box>
+                                        </Box>
+                                    ))}
+                                </Box>
+                            )}
+                        </ImageUploading>
+                    </Box>
+                    <TextField
+                        id="outlined-basic"
+                        label="Name"
+                        variant="outlined"
+                        size="small"
+                        sx={{ mb: 3 }}
+                        fullWidth
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Phone number"
+                        variant="outlined"
+                        size="small"
+                        sx={{ mb: 3 }}
+                        fullWidth
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="National ID"
+                        variant="outlined"
+                        size="small"
+                        sx={{ mb: 3 }}
+                        fullWidth
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Address"
+                        variant="outlined"
+                        size="small"
+                        sx={{ mb: 3 }}
+                        fullWidth
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Emergency Contact"
+                        variant="outlined"
+                        size="small"
+                        sx={{ mb: 3 }}
+                        fullWidth
+                    />
+                    <Button variant="contained" onClick={handleClose}>Submit</Button>
+                </Box>
             </Box>
         </Modal>
     );
--- src/Pages/Components/SalesmanTable.js
+++ src/Pages/Components/SalesmanTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,95 +6,134 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { IconButton, Tooltip } from "@mui/material";
+import { IconButton, TablePagination, Tooltip } from "@mui/material";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
-
-function createData(name, phone, nid, address, emergency) {
-    return { name, phone, nid, address, emergency };
-}
-
-const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+import data from "../../Assets/salesman.json";
 
 export default function SalesmanTable() {
-    
+    // eslint-disable-next-line
+    const [salesman, setSalesman] = useState(data);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
     return (
         <Paper sx={{ width: "100%", overflow: "hidden" }}>
             <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell align="center">Phone Number</TableCell>
-                        <TableCell align="center">National ID</TableCell>
-                        <TableCell align="center">Address</TableCell>
-                        <TableCell align="center">Emergency Contact</TableCell>
-                        <TableCell align="center">Action</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
                         <TableRow
-                            key={row.name}
                             sx={{
-                                "&:last-child td, &:last-child th": {
-                                    border: 0,
-                                },
+                                bgcolor: "rgba(32, 101, 209, 0.1)",
+                                color: "#2065d1",
                             }}
                         >
-                            <TableCell component="th" scope="row">
-                                {row.name}
+                            <TableCell sx={{ color: "#2065d1" }}>
+                                Name
                             </TableCell>
-                            <TableCell align="center">{row.phone}</TableCell>
-                            <TableCell align="center">{row.nid}</TableCell>
-                            <TableCell align="center">{row.address}</TableCell>
-                            <TableCell align="center">
-                                {row.emergency}
+                            <TableCell sx={{ color: "#2065d1" }} align="center">
+                                Phone Number
                             </TableCell>
-                            <TableCell align="center">
-                                <Tooltip title="View">
-                                    <IconButton
-                                        color="default"
-                                        aria-label="open drawer"
-                                        edge="start"
-                                        sx={{ mr: 2 }}
-                                    >
-                                        <SearchOutlinedIcon />
-                                    </IconButton>
-                                </Tooltip>
-                                <Tooltip title="Edit">
-                                    <IconButton
-                                        color="default"
-                                        aria-label="open drawer"
-                                        edge="start"
-                                        sx={{ mr: 2 }}
-                                    >
-                                        <CreateOutlinedIcon />
-                                    </IconButton>
-                                </Tooltip>
-                                <Tooltip title="Remove">
-                                    <IconButton
-                                        color="default"
-                                        aria-label="open drawer"
-                                        edge="start"
-                                        sx={{ mr: 2 }}
-                                    >
-                                        <DeleteOutlineOutlinedIcon />
-                                    </IconButton>
-                                </Tooltip>
+                            <TableCell sx={{ color: "#2065d1" }} align="center">
+                                National ID
+                            </TableCell>
+                            <TableCell sx={{ color: "#2065d1" }} align="center">
+                                Address
+                            </TableCell>
+                            <TableCell sx={{ color: "#2065d1" }} align="center">
+                                Emergency Contact
+                            </TableCell>
+                            <TableCell sx={{ color: "#2065d1" }} align="center">
+                                Action
                             </TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {salesman
+                            .slice(
+                                page * rowsPerPage,
+                                page * rowsPerPage + rowsPerPage
+                            )
+                            .map((salesman) => (
+                                <TableRow
+                                    key={salesman.name}
+                                    sx={{
+                                        "&:last-child td, &:last-child th": {
+                                            border: 0,
+                                        },
+                                    }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {salesman.name}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {salesman.phone}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {salesman.nid}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {salesman.address}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {salesman.emergency}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <Tooltip title="View">
+                                            <IconButton
+                                                color="default"
+                                                aria-label="open drawer"
+                                                edge="start"
+                                                sx={{ mr: 2 }}
+                                            >
+                                                <SearchOutlinedIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                        <Tooltip title="Edit">
+                                            <IconButton
+                                                color="default"
+                                                aria-label="open drawer"
+                                                edge="start"
+                                                sx={{ mr: 2 }}
+                                            >
+                                                <CreateOutlinedIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                        <Tooltip title="Remove">
+                                            <IconButton
+                                                color="default"
+                                                aria-label="open drawer"
+                                                edge="start"
+                                                sx={{ mr: 2 }}
+                                            >
+                                                <DeleteOutlineOutlinedIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                    </TableBody>
+                </Table>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 20]}
+                    component="div"
+                    count={data.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </TableContainer>
         </Paper>
     );
 }
